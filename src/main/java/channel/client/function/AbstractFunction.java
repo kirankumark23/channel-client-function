6b117bc7964dac612f8b074d27f594cac4b3844c
prashantmp <prashant.polshettiwar@gmail.com>
2018-11-20 11:48:50 +0530

    Added business and log methods

--- src/main/java/channel/client/function/AbstractFunction.java
+++ src/main/java/channel/client/function/AbstractFunction.java
@@ -2,5 +2,27 @@ package channel.client.function;
 
 public abstract class AbstractFunction {
 
-	public abstract void execute();	
+	/**
+	 * Execute business logic
+	 */
+	public abstract void execute(ResponseMessageEntity pResponse, CustomThreadLocal pCustomThreadLocal);	
+	
+	/**
+	 * Implement tasks to be completed before business logic execution
+	 * <li> Logging
+	 * <li> Initialization
+	 */
+	public abstract void prolog(ResponseMessageEntity pResponse, CustomThreadLocal pCustomThreadLocal);
+
+	/**
+	 * Implement tasks to be completed after business logic execution
+	 * <li> Logging
+	 * <li> Resource deallocation
+	 */	
+	public abstract void epilog(ResponseMessageEntity pResponse, CustomThreadLocal pCustomThreadLocal);
+
+	/**
+	 * In the event of an exception, this function will be called to log all contextual data, to help with investigation
+	 */
+	public abstract void errorlog(ResponseMessageEntity pResponse, CustomThreadLocal pCustomThreadLocal);
 }
