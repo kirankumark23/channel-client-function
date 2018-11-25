8e167dc839758ad3663cdc1796c44fcbec51cbc3
Gauri Shankar Choudhary <shankargaurisujit@gmail.com>
2018-11-25 16:21:15 +0530

    CBF-35

--- src/main/java/channel/client/function/CommonConstants.java
+++ src/main/java/channel/client/function/CommonConstants.java
@@ -3,4 +3,6 @@ package channel.client.function;
 public class CommonConstants {
 	public static final String CURRENT_USER="currentUser";
 	public static final String VERSION_STRING="mVersion";
+	public static final String GOOGLE_TOKEN_STRING="google :";
+	public static final String CHANNEL_TOKEN_STRING="channel:";
 }
